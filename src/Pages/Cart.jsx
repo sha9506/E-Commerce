90c6b035e8745ea72864e17fb41fdcae303b4d73
sabnam <sabnamhazari@gmail.com>
2024-05-02 23:25:09 +0530

    cart

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,67 +1,102 @@
 import React from "react";
-import { useEffect , useState} from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Footer from "../Molecules/Footer/Footer";
 import Navbar from "../Molecules/Navbar/Navbar";
 import Modal from "../Molecules/Order_Modal";
 
-const Cart=()=>
-{
-    const [showModal,setShowModal]= useState(false)
-    const navigate= useNavigate();
-    useEffect(()=>
-    {
-        let details= localStorage.getItem('user');
-        if(!details)
-        {
-            navigate("/");
+const Cart = () => {
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+  const [productList, setProductList] = useState([]);
+  const [total, setTotal] = useState(0);
+  useEffect(() => {
+    let details = localStorage.getItem("user");
+    if (!details) {
+      navigate("/");
+    }
+    if (localStorage.getItem("cart_list")) {
+      setProductList(JSON.parse(localStorage.getItem("cart_list")));
+      let a = 0;
+      JSON.parse(localStorage.getItem("cart_list")).map((item) => {
+        if (item.added_to_cart) {
+          a = a + item.product_price;
         }
-    },[])
+        setTotal(a);
+      });
+    }
+  }, []);
 
-    return(<div>
-        {
-            showModal ?<Modal onclose={()=>{setShowModal(false)}}/> : null
-        }
-        <Navbar/>
-            <div className="cart-bg">
-                <div className="cart-header">
-                    <div>Product</div>
-                    <div>Price</div>
-                    <div>Quantity</div>
-                    <div>Subtotal</div>
-                </div>
-                <div className="cart-item">
-                    <div>Printed top</div>
-                    <div>Rs.650.00</div>
-                    <div>2</div>
-                    <div>Rs.1300.00</div>
-                </div>
-                <div className="cart-button-box" >
-                    <button className="cart-button">Update Cart</button>
-                    <button className="cart-button" onClick={()=>(navigate('/landing'))}>Return to Shop</button>
-                </div>
-                <div className="cart-check">
-                    <div className="cart-total">Cart Total</div>
-                    <table >
-                        <tr>
-                            <td>Subtotal</td>
-                            <td>89932 Rs</td>
-                        </tr>
-                        <tr>
-                            <td>Shipping</td>
-                            <td>Free</td>
-                        </tr>
-                        <tr>
-                            <td>Total: </td>
-                            <td>89932 Rs</td>
-                        </tr>
-                    </table>
-                    <button className="checkout" onClick={()=>{setShowModal(true)}}>Proceed to Order</button>
-                </div>
-            </div>
-            
-        <Footer/>
-    </div>)
-}
+  return (
+    <div>
+      {showModal ? (
+        <Modal
+          onclose={() => {
+            setShowModal(false);
+          }}
+        />
+      ) : null}
+      <Navbar />
+      <div className="cart-bg">
+        <div className="cart-header">
+          <div>Product</div>
+          <div>Price</div>
+          <div>Quantity</div>
+          <div>Subtotal</div>
+        </div>
+        <div className="cart-box">
+          {productList
+            ? productList.map((item) => {
+                if (item.added_to_cart) {
+                  return (
+                    <div className="cart-item">
+                      <div>{item.product_name}</div>
+                      <div>Rs.{item.product_price}</div>
+                      <div>1</div>
+                      <div>Rs.{item.product_price}</div>
+                    </div>
+                  );
+                }
+              })
+            : null}
+        </div>
+
+        <div className="cart-button-box">
+          <button className="cart-button">Update Cart</button>
+          <button className="cart-button" onClick={() => navigate("/landing")}>
+            Return to Shop
+          </button>
+        </div>
+        <div className="cart-check">
+          <div className="cart-total">Cart Total</div>
+          <table>
+            <tr>
+              <td>Subtotal</td>
+              <td>{total} Rs</td>
+            </tr>
+            <tr>
+              <td>Shipping</td>
+              <td>Free</td>
+            </tr>
+            <tr>
+              <td>Total: </td>
+              <td>{total} Rs</td>
+            </tr>
+          </table>
+          <button
+            className="checkout"
+            onClick={() => {
+              setShowModal(true);
+            }}
+          >
+            Proceed to Order
+          </button>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default Cart;
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -5,55 +5,81 @@ import Footer from "../Molecules/Footer/Footer";
 import Navbar from "../Molecules/Navbar/Navbar";
 import Product_list from "../DummyData/Product_list";
 
-const Products=()=>
-{
-    const[productList, setProductList]=useState([]);
-    const navigate=useNavigate();
-    const [searchParam]=useSearchParams();
-    useEffect(()=>
-    {
-        let details= localStorage.getItem('user');
-        if(!details)
-        {
-            navigate("/");
-        }
+const Products = () => {
+  const [productList, setProductList] = useState([]);
+  const navigate = useNavigate();
+  const [searchParam] = useSearchParams();
+  useEffect(() => {
+    let details = localStorage.getItem("user");
+    if (!details) {
+      navigate("/");
+    }
 
-        if(searchParam.get('search'))
-        {
-            const filterBySearch = Product_list.filter((item)=> 
-            item.product_name.toLowerCase().includes(searchParam.get('search').toLowerCase())
-                );
-            setProductList(filterBySearch);
-        }
-        else
-        {
-            setProductList(Product_list);
-        }    
-    },[])
+    if (searchParam.get("search")) {
+      const filterBySearch = Product_list.filter((item) =>
+        item.product_name
+          .toLowerCase()
+          .includes(searchParam.get("search").toLowerCase())
+      );
+      setProductList(filterBySearch);
+    } else {
+      setProductList(Product_list);
+    }
+  }, []);
 
+  const addToCart = (data) => {
+    let a = [];
+    data.added_to_cart = !data.added_to_cart;
+    productList.map((item) => {
+      if (data.product_name == item.product_name) {
+        a[a.length] = data;
+      } else {
+        a[a.length] = item;
+      }
+    });
+    setProductList(a);
+    localStorage.setItem("cart_list", JSON.stringify(a));
+  };
 
-    return(<div>
-        <Navbar/>
-            <div className="product-objects">
-            {
-                productList.length >0? (productList.map((item)=>
-                {
-                    return(
-                        <div className="product-list" onClick={()=>(navigate('/cart'))}>
-                            <img src={item.icon} alt="product" className="product-icon"></img>
-                             <div className="product-info">
-                                <div className="product-title">{item.product_name}</div>
-                                <div className="product-price">Rs. {item.product_price}</div>
-                             </div>
-                        </div>
-                    )
-                })) :
-                <div>We Don't Have That In Our Collection.</div>
-            }
-           
-            </div>
-        <Footer/>
-    </div>)
-}
+  return (
+    <div>
+      <Navbar />
+      <div className="product-objects">
+        {productList.length > 0 ? (
+          productList.map((item) => {
+            return (
+              <div
+                className="product-list"
+                onClick={() => {
+                  addToCart(item);
+                }}
+              >
+                <img
+                  src={item.icon}
+                  alt="product"
+                  className="product-icon"
+                ></img>
+                <div
+                  className="product-info"
+                  style={item.added_to_cart ? { backgroundColor: "green" } : {}}
+                >
+                  {item.added_to_cart ? null : (
+                    <div className="product-title">{item.product_name}</div>
+                  )}
+                  <div className="product-price">
+                    {item.added_to_cart ? "Added" : `Rs. ${item.product_price}`}
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        ) : (
+          <div>We Don't Have That In Our Collection.</div>
+        )}
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Products;
